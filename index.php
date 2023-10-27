7e571f168ba7bd2e18ab71dd8d9ed928f87d0eb4
senathn <senathngamage@gmail.com>
2023-10-27 22:31:50 +0530

    table updates

--- index.php
+++ index.php
@@ -10,5 +10,25 @@
 
     <p>Functionality testing has started now.</p>
     <b style="width:100%;font-size:large;">TEST</b>
+
+    <style>
+        table, td, tr, th {
+            border: 1px solid black;
+        }
+    </style>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>message</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Hello</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Bye!</td>
+        </tr>
+    </table>
 </body>
 </html>
