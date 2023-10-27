5886dce38794ba11da61c7b78bc86eaa74df5b5a
senathn <senathngamage@gmail.com>
2023-10-27 22:04:48 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Functionality</title>
+</head>
+<body>
+    <h3>Functionality test</h3>
+
+    <p>Test sentence</p>
+</body>
+</html>
